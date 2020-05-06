a764e577882798d9ba03713b6ae321f8881cf6a0
Lee <igni950424@gmail.com>
2020-05-06 15:08:33 +0900

    GIT TEST.JAVA 2nd

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 class Test{
 	public static void main(String [] args){
 	//	System.out.println("Hell");
-		System.out.println("Hell");
+	//	System.out.println("Hell");
 		System.out.println("BRANCH1");
 		System.out.println("BRANCH2");
 	}
