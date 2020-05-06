838140c9d714ba51a86e129c08dcebb5d3f90958
Lee <igni950424@gmail.com>
2020-05-06 15:01:54 +0900

    NEW REPO TEST.JAVA

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class Test{
 	public static void main(String [] args){
-		System.out.println("Hell");
+	//	System.out.println("Hell");
 		System.out.println("Hell");
 		System.out.println("BRANCH1");
 		System.out.println("BRANCH2");
